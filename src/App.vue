86068cd7857f9acd9c1bddcea4a08ab26aa1a933
VirtualZer0 <virtualos@list.ru>
2022-11-20 21:26:48 +0300

    Revert packages update

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
           <input type="file" id="input" @change="setInput" multiple accept="image/png, image/gif, image/jpeg" />
         </div>
 
-        <button class="round-button" @click="colors.length = 0">
+        <button class="round-button" @click="colors.length = 0; savePalette();">
           <svg viewBox="0 0 24 24">
             <path fill="currentColor"
               d="M3.27,1L2,2.27L6.22,6.5L3,9L4.63,10.27L12,16L14.1,14.37L15.53,15.8L12,18.54L4.63,12.81L3,14.07L12,21.07L16.95,17.22L20.73,21L22,19.73L3.27,1M19.36,10.27L21,9L12,2L9.09,4.27L16.96,12.15L19.36,10.27M19.81,15L21,14.07L19.57,12.64L18.38,13.56L19.81,15Z" />
@@ -77,7 +77,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, nextTick, ref } from 'vue';
+import { defineComponent, nextTick, ref, onMounted } from 'vue';
 import rgbHex from 'rgb-hex';
 import hexRgb, { RgbaObject } from 'hex-rgb';
 
@@ -149,6 +149,7 @@ export default defineComponent({
 
         setTimeout(() => {
           inputProgress.value = false;
+          savePalette();
         }, 150);
       }, 150)
     };
@@ -218,6 +219,21 @@ export default defineComponent({
       link.click();
     };
 
+    const savePalette = () => {
+      localStorage.setItem('palette', JSON.stringify(colors.value));
+    }
+
+    const loadPalette = () => {
+      const palette = localStorage.getItem('palette');
+      if (palette) {
+        colors.value = JSON.parse(palette);
+      }
+    }
+
+    onMounted(() => {
+      loadPalette();
+    });
+
     return {
       outputImg,
       colors,
@@ -226,7 +242,8 @@ export default defineComponent({
       outputProgress,
       setInput,
       setOutput,
-      downloadImage
+      downloadImage,
+      savePalette
     };
 
   }
